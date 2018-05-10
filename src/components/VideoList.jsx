62ac9a8f45530feba452052948890bc7e10caa7f
xueccc <xueqingc@gmail.com>
2018-05-09 21:01:53 -0700

    Passed tests for VideoList, VideoListEntry, VideoPlayer, and searchYoutube

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,10 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div>
+      {props.videos.map(video => 
+        <VideoListEntry handler={props.handler} video={video}/>
+      )}
+    </div>
   </div>
 );
 
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax ({
+    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video',
+    type: 'GET',
+    data: {
+      key: options.key,
+      q: options.query,
+      maxResults: options.max
+    },
+    success: data => callback(data.items)
+  });
 };
 
 window.searchYouTube = searchYouTube;
